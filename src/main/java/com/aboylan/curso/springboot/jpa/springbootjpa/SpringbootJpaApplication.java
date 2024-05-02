a07852f218632f9ca7d1ec22fd02fb50c85da1fe
Alex Boylan <aboylan@users.noreply.github.com>
2024-05-01 21:30:40 -0600

    Crear y la anotacion @Transactional

--- src/main/java/com/aboylan/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/aboylan/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -2,11 +2,13 @@ package com.aboylan.curso.springboot.jpa.springbootjpa;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.aboylan.curso.springboot.jpa.springbootjpa.entities.Person;
 import com.aboylan.curso.springboot.jpa.springbootjpa.repositories.PersonRepository;
@@ -26,13 +28,27 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		create();
 	}
 
+	@Transactional
 	public void create() {
-		Person person = new Person(null, "Lalo", "Thor", "Python");
-		
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el nombre:");
+		String name = scanner.next();
+		System.out.println("Ingrese el apellido:");
+		String lastname = scanner.next();
+		System.out.println("Ingrese el lenguaje de programacion:");
+		String programmingLanguage = scanner.next();
+		scanner.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage);
+
 		Person personNew = repository.save(person);
 		System.out.println(personNew);
+
+		repository.findById(personNew.getId()).ifPresent(System.out::println);
 	}
 
+	@Transactional(readOnly = true)
 	public void findOne() {
 		// Person person = null;
 		// Optional<Person> optionalPerson = repository.findById(1L);
@@ -44,6 +60,7 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		repository.findByNameContaining("hn").ifPresent(System.out::println);
 	}
 
+	@Transactional(readOnly = true)
 	public void list() {
 		// List<Person> persons = (List<Person>) repository.findAll();
 		// List<Person> persons = (List<Person>)
