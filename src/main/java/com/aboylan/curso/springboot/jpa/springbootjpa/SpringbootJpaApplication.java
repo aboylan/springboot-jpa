9301d963904a026f8b3c573cc32e9debd6784969
Alex Boylan <aboylan@users.noreply.github.com>
2024-06-07 21:21:47 -0600

    Usando between basado en el nombre del metodo (Query Method)

--- src/main/java/com/aboylan/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/aboylan/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -32,10 +32,10 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 	@Transactional(readOnly = true)
 	public void personalizedQueriesBetween() {
 		System.out.println("================== Consultas por rangos ==================");
-		List<Person> persons = repository.findAllBetweenId();
+		List<Person> persons = repository.findByIdBetween(2L, 5L);
 		persons.forEach(System.out::println);
 
-		persons = repository.findAllBetweenName();
+		persons = repository.findByNameBetween("J", "Q");
 		persons.forEach(System.out::println);
 	}
 
