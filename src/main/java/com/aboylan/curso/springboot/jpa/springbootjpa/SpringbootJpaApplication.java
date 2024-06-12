577437b1f1d03f972a570438fde0da63693d0bf3
Alex Boylan <aboylan@users.noreply.github.com>
2024-06-11 21:27:30 -0600

    Subqueries o subconsultas en JPQL

--- src/main/java/com/aboylan/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/aboylan/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -26,7 +26,23 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		queriesFunctionAggregation();
+		subQueries();
+	}
+
+	@Transactional(readOnly = true)
+	public void subQueries() {
+
+		System.out.println("================== Consulta por el nombre mas corto y su largo ==================");
+		List<Object[]> registers = repository.getShorterName();
+		registers.forEach(reg -> {
+			String name = (String) reg[0];
+			Integer length = (Integer) reg[1];
+			System.out.println("name=" + name + ", length=" + length);
+		});
+
+		System.out.println("================== Consulta para obtener el ultimo registro de persona ==================");
+		Optional<Person> optionalPerson = repository.getLastRegistration();
+		optionalPerson.ifPresent(System.out::println);
 	}
 
 	@Transactional(readOnly = true)
